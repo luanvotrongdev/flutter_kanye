38f624e6dacfb5429b5c02a3086d35a53a19cadb
luan.votrongdev <luan.votrongdev@gmail.com>
2019-03-24 11:33:17 +0900

    + update mainbloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+//import 'package:url_launcher/url_launcher.dart';
+import 'package:rxdart/rxdart.dart';
+//import 'package:clipboard_manager/clipboard_manager.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Kanye',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -44,17 +48,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
+  var _dio = Dio();
+  PublishSubject _quotePublishObject = PublishSubject();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _showSnackbar() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Copied to clipboard"),
+    ));
+  }
+
+  void _requestQuote() {
+    _dio.get("https://api.kanye.rest/").then((resp) {
+      if (resp.statusCode == 200) {
+        Map<String, dynamic> responseData = resp.data;
+        _quotePublishObject.add(responseData["quote"]);
+      }
     });
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _requestQuote();
+    _quotePublishObject.startWith("fsafsd");
+  }
+
+  Widget _buildBody(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Padding(
+      padding: EdgeInsets.fromLTRB(5, 30, 5, 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: size.width / 3,
+            height: size.width / 3,
+            child: Image.asset("images/logo.png"),
+          ),
+          FlatButton(
+              onPressed: () async {
+//                const url = 'https://github.com/ajzbc/kanye.rest';
+//                if (await canLaunch(url)) {
+//                  await launch(url);
+//                } else {
+//                  throw 'Could not launch $url';
+//                }
+              },
+              child: const Text("kanye.rest")),
+          Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      OutlineButton(
+                        child: const Text("Facebook"),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                      ),
+                      OutlineButton(
+                        child: const Text("Copy"),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                                onPressed: ()
+                                {
+                                  _showSnackbar();
+                                },
+                      ),
+                      OutlineButton(
+                        child: const Text("Refresh"),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        onPressed: _requestQuote,
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                  StreamBuilder(
+                    stream: _quotePublishObject,
+                    initialData: "",
+                    builder: (context, snapshot) {
+                      String str = "";
+                      if (snapshot.data != null) str = snapshot.data;
+                      return Text('"$str"');
+                    },
+                  )
+                ],
+              ))
+        ],
+      ),
+    ));
   }
 
   @override
@@ -66,46 +162,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      key: _scaffoldKey,
+      body: _buildBody(
+          context), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
